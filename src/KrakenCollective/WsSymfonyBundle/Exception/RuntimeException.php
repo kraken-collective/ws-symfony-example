887e0ba7ef885391aa0f039794caafa73fa1afdd
Michał Kurzeja <mike.kurzeja@gmail.com>
2016-11-25 16:29:28 +0100

    extracted ws-symfony component to a separate bundle

--- src/KrakenCollective/WsSymfonyBundle/Exception/RuntimeException.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace KrakenCollective\WsSymfonyBundle\Exception;
-
-class RuntimeException extends WsSymfonyException
-{
-}
--- src/KrakenCollective/WsSymfonyBundle/Exception/WsSymfonyException.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace KrakenCollective\WsSymfonyBundle\Exception;
-
-use Exception;
-
-class WsSymfonyException extends Exception
-{
-}
